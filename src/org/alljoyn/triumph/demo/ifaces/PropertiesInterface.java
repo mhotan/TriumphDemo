621efa1321f1542e6d45cc49ae5a69b658aad304
Mike Hotan <mhotan@quicinc.com>
2013-08-15 17:01:24 -0700

    Fixed PropertiesInterface
    Added @BusInterface annotation

--- src/org/alljoyn/triumph/demo/ifaces/PropertiesInterface.java
+++ src/org/alljoyn/triumph/demo/ifaces/PropertiesInterface.java
@@ -1,8 +1,10 @@
 package org.alljoyn.triumph.demo.ifaces;
 
 import org.alljoyn.bus.BusException;
+import org.alljoyn.bus.annotation.BusInterface;
 import org.alljoyn.bus.annotation.BusProperty;
 
+@BusInterface
 public interface PropertiesInterface {
 
 	
